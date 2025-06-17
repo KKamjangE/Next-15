8ec00db586ef539768325fe31d9e66ac44e5d46d
KKamjangE <ajm980718@gmail.com>
2025-06-17 15:36:37 +0900

    home layout sidebar UI 구현

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavbar } from "@/modules/home/ui/components/home-navbar";
+import { HomeSidebar } from "@/modules/home/ui/components/home-sidebar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,7 +11,10 @@ export const Homelayout = ({ children }: LayoutProps) => {
     <SidebarProvider>
       <div className="w-full">
         <HomeNavbar />
-        <div>{children}</div>
+        <div className="flex min-h-screen pt-[4rem]">
+          <HomeSidebar />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
       </div>
     </SidebarProvider>
   );
