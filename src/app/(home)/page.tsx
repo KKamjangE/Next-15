8ac55a0ef8d2ea0bf236b7071a2245f8216bb255
KKamjangE <ajm980718@gmail.com>
2025-06-24 16:57:08 +0900

    suspense, errorboundary, hydrateclient로 client 컴포넌트 감싸기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,18 @@
 import PageClient from "@/app/(home)/client";
-import { trpc } from "@/trpc/server";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
   void trpc.hello.prefetch({ text: "Antonio" });
 
   return (
-    <div>
-      <PageClient />
-    </div>
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <PageClient />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
   );
 }
