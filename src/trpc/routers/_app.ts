4431f3aa3f367a5675c4c03da3d9ee08e66de70e
KKamjangE <ajm980718@gmail.com>
2025-06-25 18:02:44 +0900

    카테고리 tRPC router 생성

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,8 @@
-import { z } from "zod";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { createTRPCRouter } from "../init";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+
 export const appRouter = createTRPCRouter({
-  hello: protectedProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    ) // 입력 스키마
-    .query((opts) => {
-      console.log({ dbUser: opts.ctx.user });
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }), // 쿼리 핸들러
+  categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
