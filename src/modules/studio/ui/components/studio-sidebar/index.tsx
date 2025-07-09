e9a71c4c732b5cee7497b9a652a7f52e0d7e7727
KKamjangE <ajm980718@gmail.com>
2025-07-09 14:14:22 +0900

    studio sidebar 컴포넌트 구현

--- src/modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,15 +1,49 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import { MainSection } from "@/modules/home/ui/components/home-sidebar/main-section";
-import { PersonalSection } from "@/modules/home/ui/components/home-sidebar/personal-section";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const StudioSidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+    <Sidebar className="pt-16 z-40" collapsible="icon">
       <SidebarContent className="bg-background">
-        <MainSection />
-        <Separator />
-        <PersonalSection />
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                isActive={pathname === "/studio"}
+                tooltip="Exit studio"
+                asChild
+              >
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <Separator />
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip="Exit studio" asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
