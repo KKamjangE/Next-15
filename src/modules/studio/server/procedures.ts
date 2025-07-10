ee11db4ac372787917e5cfeb285ccfcd873b29b2
KKamjangE <ajm980718@gmail.com>
2025-07-10 15:11:00 +0900

    prefetchInfinite에 맞게 studio router 구성

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,11 +1,60 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, desc, eq, lt, or } from "drizzle-orm";
+import { z } from "zod";
 
 export const studioRouter = createTRPCRouter({
-  getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(videos);
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        // 어디서부터 가져올지 알려주는 값
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        // 한번에 가져오는 데이터의 개수
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
+      const data = await db
+        .select()
+        .from(videos)
+        .where(
+          and(
+            eq(videos.userId, userId),
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt), // 이전 시간보다 오래된 것
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt), // 같은 시간이라면
+                    lt(videos.id, cursor.id) // ID가 더 작은 것
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id)) // 수정 시간 내림차순, ID 내림차순
+        // Add 1 to the limit to check if there is more data
+        .limit(limit + 1);
 
-    return data;
-  }),
+      const hasMore = data.length > limit;
+      // Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+      // Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return { items, nextCursor };
+    }),
 });
