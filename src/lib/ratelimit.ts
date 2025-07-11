c5d69d5f1a02345b401e504b1f4e0f21866009ad
KKamjangE <ajm980718@gmail.com>
2025-07-11 14:27:24 +0900

    ratelimit 100개로 수정

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -3,5 +3,5 @@ import { Ratelimit } from "@upstash/ratelimit";
 
 export const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(10, "10s"), // 10초 이내 10개의 요청이 발생하면 타임아웃
+  limiter: Ratelimit.slidingWindow(100, "10s"), // 10초 이내 100개의 요청이 발생하면 타임아웃
 });
