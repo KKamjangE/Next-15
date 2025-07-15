1cfedc08bbe3b463008bb465648f128302deee41
KKamjangE <ajm980718@gmail.com>
2025-07-15 16:21:10 +0900

    Remove debug log from formatDuration function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const formatDuration = (duration: number) => {
-  console.log(duration);
-
   const seconds = Math.floor((duration & 60000) / 1000);
   const minutes = Math.floor(duration / 60000);
   return `${minutes.toString().padStart(2, "0")}:${seconds
