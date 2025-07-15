318dcee0e87da4fe279b2782628a5e8dbcdb0ba3
KKamjangE <ajm980718@gmail.com>
2025-07-15 15:49:22 +0900

    video thumbnail에 gif, duration 추가

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,20 +1,41 @@
+import { formatDuration } from "@/lib/utils";
 import Image from "next/image";
 
-export const VideoThumbnail = () => {
+interface VideoThumbnailProps {
+  title: string;
+  duration: number;
+  imageUrl?: string | null;
+  previewUrl?: string | null;
+}
+
+export const VideoThumbnail = ({
+  title,
+  imageUrl,
+  previewUrl,
+  duration,
+}: VideoThumbnailProps) => {
   return (
-    <div className="relative">
+    <div className="relative group">
       {/* Thumbnail wrapper */}
       <div className="relative w-full overflow-hidden rounded-xl aspect-video">
         <Image
-          src="/placeholder.svg"
-          alt="Thumbnail"
+          src={imageUrl ?? "/placeholder.svg"}
+          alt={title}
           fill
-          className="size-full object-cover"
+          className="size-full object-cover group-hover:opacity-0"
+        />
+        <Image
+          src={previewUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="size-full object-cover opacity-0 group-hover:opacity-100"
         />
       </div>
 
       {/* Video duration box */}
-      {/* TODO: Add video duration box */}
+      <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/50 text-white text-xs font-medium">
+        {formatDuration(duration)}
+      </div>
     </div>
   );
 };
